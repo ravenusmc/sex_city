031ffadde85406618f0ebd0c3653f523195cef26
ravenusmc <mcuddy77@gmail.com>
2019-10-07 10:03:31 -0400

    worked on header for landing page

--- client/src/components/home/Header.vue
+++ client/src/components/home/Header.vue
@@ -2,7 +2,9 @@
   <div>
 
     <header>
-      <h1>Home Page</h1>
+      <div class='homeHeader'>
+        <h1 class='font headerTitle'>Sex and the City Study</h1>
+      </div>
     </header>
 
   </div>
@@ -15,7 +17,33 @@ export default {
 </script>
 
 <style scoped>
+
+.font {
+  font-family: 'Indie Flower', cursive;
+}
+
 header {
+  background-image: url('../../assets/img/header_image.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.homeHeader {
+  border-radius: 10px;
+  padding: 25px;
+  background-color: rgba(0, 0, 0, 0.7);
   border: 2px solid black;
+}
+
+.headerTitle {
+  text-transform: uppercase;
+  color: white;
 }
 </style>
