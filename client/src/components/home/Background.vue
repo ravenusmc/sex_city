dc93fa95aedc3b9bdb9438883e3a79165082e7f2
ravenusmc <mcuddy77@gmail.com>
2019-10-08 15:55:52 -0400

    started work on a new component

---
+++ client/src/components/home/Background.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    background Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Background',
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header />
+    <Background />
   </div>
 </template>
 
 <script>
 import Header from '@/components/home/Header.vue';
+import Background from '@/components/home/Background.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
+    Background
   },
 };
 </script>
