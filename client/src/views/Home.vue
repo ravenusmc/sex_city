13ea6a27ddf3f3da1af23a280e762ab0206841ec
ravenusmc <mcuddy77@gmail.com>
2019-10-12 18:50:51 -0400

    working on main component for home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <Header />
     <Background />
+    <Main />
   </div>
 </template>
 
 <script>
 import Header from '@/components/home/Header.vue';
 import Background from '@/components/home/Background.vue';
+import Main from '@/components/home/Main.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
-    Background
+    Background,
+    Main,
   },
 };
 </script>
