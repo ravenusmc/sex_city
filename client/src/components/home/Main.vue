61fd9f9e7060a129f2231400c149d4a5d570eeb4
ravenusmc <mcuddy77@gmail.com>
2019-10-13 18:06:18 -0400

    adding to main component on home page

--- client/src/components/home/Main.vue
+++ client/src/components/home/Main.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    Main
+
+    <main>
+      <h1 class='font center'>Study Area</h1>
+
+      <section id='firstStudyArea'>
+
+        <div>
+        </div>
+
+        <div>
+          <form>
+          </form>
+        </div>
+
+      </section>
+
+    </main>
+
   </div>
 </template>
 
@@ -11,4 +28,24 @@ export default {
 </script>
 
 <style scoped>
+
+.center {
+  text-align: center;
+}
+
+.font {
+  font-family: 'Indie Flower', cursive;
+}
+
+main {
+  margin-top: 50px;
+}
+
+#firstStudyArea {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2em;
+  border: 2px solid black;
+}
+
 </style>
