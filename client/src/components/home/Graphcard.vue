ef485a8d8c0da06c1b5bb4dc0116c0ed22923d2e
ravenusmc <mcuddy77@gmail.com>
2019-10-14 14:22:05 -0400

    started work on the graph card

---
+++ client/src/components/home/Graphcard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+    <div class='cardDiv'>
+      <v-card>
+        <GChart
+          :type="typeOne"
+          :data="data"
+          :options="options"
+        />
+      </v-card>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  name: 'GraphCard',
+  props: ['typeOne', 'data', 'options'],
+}
+</script>
+
+<style scoped>
+.cardDiv {
+  margin: 10px;
+}
+</style>
