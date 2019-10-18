43fc07d07378420b788fa5f32c137f80a01cca8e
ravenusmc <mcuddy77@gmail.com>
2019-10-18 15:46:10 -0400

    added first three seasons of word cloud images to home page

--- client/src/components/home/Main.vue
+++ client/src/components/home/Main.vue
@@ -42,6 +42,30 @@
 
       <section id='secondStudyArea'>
 
+        <div>
+          <img :src="image" alt='Word Cloud'>
+        </div>
+
+        <div class='form_div'>
+
+          <form @submit="changeWordCloud">
+            <h3 class='font'>Select Season:</h3>
+            <select v-model="season" name="season">
+              <option v-for="season in seasons" :value="season">{{ season }}</option>
+            </select>&nbsp;
+            <button type="submit">Submit</button>
+          </form>
+
+          <div>
+            <p class='form_paragraph'>
+              This area will allow the user to select a season and then see a
+              word cloud that will show the most common words from that season. The
+              word cloud comes from every line that was spoken in the season.
+            </p>
+          </div>
+
+        </div>
+
 
       </section>
 
@@ -55,6 +79,9 @@
 import { mapActions } from 'vuex';
 import { mapGetters } from 'vuex';
 import GraphCard from '@/components/graphs/Graphcard.vue';
+import Image1 from '@/assets/img/season1.png';
+import Image2 from '@/assets/img/season2.png';
+import Image3 from '@/assets/img/season3.png';
 
 export default {
   name: 'Main',
@@ -66,6 +93,8 @@ export default {
       season: '1',
       seasons: ['1', '2', '3', '4', '5', '6'],
       typeOne: "BarChart",
+      image: Image1,
+      image2: Image2,
       chartOptionsSeasonLinesChart: {
         title: 'Lines By Season By Character',
         legend: { position: 'bottom' },
@@ -92,6 +121,17 @@ export default {
         season: this.season,
       }
       this.fetchSeasonData({payload})
+    },
+    changeWordCloud(evt) {
+      evt.preventDefault();
+      console.log(this.season)
+      if (this.season === '1'){
+        this.image = Image1
+      }else if (this.season === '2'){
+        this.image = Image2
+      }else if (this.season === '3'){
+        this.image = Image3
+      }
     }
   },
   mounted() {
@@ -148,6 +188,18 @@ button {
 button:hover {
   background-color: #de547d;
   border: #de547d;
+}
+
+#secondStudyArea {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2em;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+
+img {
+  height: 300px;
 }
 
 @media only all and (max-width: 800px) {
--- server/lang.py
+++ server/lang.py
@@ -10,24 +10,10 @@ class Lang():
     def __init__(self):
         self.data = pd.read_csv('./data/SATC_all_lines.csv')
 
-    def test(self):
-        my_stopwords = set(STOPWORDS)
-        two_cities = """It was the best of times, it was the worst of times, it was the age of
-        wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of
-        incredulity, it was the season of Light, it was the season of Darkness, it was the spring
-        of hope, it was the winter of despair, we had everything before us, we had nothing before
-        us, we were all going direct to Heaven, we were all going direct the other way—in short,
-        the period was so far like the present period, that some of its noisiest authorities insisted
-        on its being received, for good or for evil, in the superlative degree of comparison only."""
-        my_cloud = WordCloud(background_color='white', stopwords=my_stopwords).generate(two_cities)
-        plt.imshow(my_cloud, interpolation='bilinear')
-        plt.axis('off')
-        plt.show()
-
     def seasonWordCloud(self, season):
         season_data_set = self.data[(self.data.Season == season)]
         count = 0
-        #This is the specific place that 
+        #This is the specific place that
         line_count = season_data_set.index[0]
         lines = []
         while count < len(season_data_set):
@@ -47,33 +33,4 @@ class Lang():
 
 
 lang = Lang()
-# lang.test()
-lang.seasonWordCloud(6)
-
-#example 1
-## Define the stopwords list
-# my_stopwords = set(STOPWORDS)
-# my_stopwords.update(['movie', 'movies', 'film', 'films', 'watch', 'br'])
-
-#Generate and show the word cloud
-# my_cloud = WordCloud(background_color='white', stopwords=my_stopwords).generate(name_string)
-# plt.imshow(my_cloud, interpolation='bilinear')
-
-### Example 2
-
-# two_cities = """It was the best of times, it was the worst of times, it was the age of
-# wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of
-# incredulity, it was the season of Light, it was the season of Darkness, it was the spring
-# of hope, it was the winter of despair, we had everything before us, we had nothing before
-# us, we were all going direct to Heaven, we were all going direct the other way—in short,
-# the period was so far like the present period, that some of its noisiest authorities insisted
-# on its being received, for good or for evil, in the superlative degree of comparison only."""
-
-#cloud = WordCloud().generate(two_cities) # ?WordCloud
-
-#my_cloud = WordCloud(background_color='white', stopwords=my_stopwords).generate(descriptions)
-
-# plt.imshow(cloud, interpolation='bilinear')
-#
-# plt.axis('off')
-# plt.show()
+lang.seasonWordCloud(3)
