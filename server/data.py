f7186feb20bf126ea15dbd9c8a0f41270bd7eaba
ravenusmc <mcuddy77@gmail.com>
2019-10-22 13:11:53 -0400

    wrote code to get sentiment of each characters line

--- server/data.py
+++ server/data.py
@@ -3,6 +3,7 @@
 #importing supporting libraries
 import numpy as np
 import pandas as pd
+from textblob import TextBlob
 
 class Data():
 
@@ -42,9 +43,36 @@ class Data():
         #Here I get a smaller data set based on where the season is set to
         #what the user wants.
         season_data_set = self.data[(self.data.Season == season)]
+        season_data = []
         #This list will hold the main characters that will be examined
         characters = ['Carrie', 'Miranda', 'Charlotte', 'Samantha']
+        columns = ['Character', 'Lines']
+        season_data.append(columns)
+        #looping through the characters list
+        for character in characters:
+            rows = []
+            #resetting the dataset each time we loop through the data.
+            character_data_set = season_data_set
+            speaker_data_set = character_data_set[(character_data_set.Speaker == character)]
+            valance_list = []
+            for line in speaker_data_set['Line']:
+                valance = TextBlob(line)
+                valance_list.append(valance.sentiment[0])
+            list_total = sum(valance_list)
+            average = list_total / len(valance_list)
+            rows.append(character)
+            rows.append(int(average))
+            season_data.append(rows)
+        print(season_data)
 
-#data = Data()
-#data.test()
-#data.seasonLineCountByCharacter(4)
+
+
+
+# text = "Today was a good day"
+# my_valance = TextBlob(text)
+# print(my_valance)
+# print(my_valance.sentiment)
+
+data = Data()
+# data.test()
+data.sentimentByCharacter(1)
